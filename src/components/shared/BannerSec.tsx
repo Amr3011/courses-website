d6680c63477a7e6dc7ff5ef3c89cc63c67f18cf4
Mohamed Seed <mosaeed7888@gmail.com>
2025-09-11 19:37:24 +0300

    feat: enhance All_PurchasedCourses page with new layout and components - added LeftSide and CardMentor component

--- src/components/shared/BannerSec.tsx
+++ src/components/shared/BannerSec.tsx
@@ -6,9 +6,9 @@ const BannerSec = ({
   paragraph?: string;
 }) => {
   return (
-    <div className=" bg-[#EDF9FF] w-full text-center flex flex-col gap-5 justify-center items-center py-9 rounded-2xl mt-4 mb-7 md:mb-16">
-      <h1 className="text-3xl md:text-6xl font-bold">{title}</h1>
-      <p className="text-xs md:text-sm font-sans">{paragraph}</p>
+    <div className=" bg-[#EDF9FF] w-full text-center flex flex-col gap-4 justify-center items-center py-9 rounded-2xl mt-4 mb-7 md:mb-16">
+      <h1 className="text-4xl md:text-6xl font-bold">{title}</h1>
+      <p className="p-5 text-base font-sans">{paragraph}</p>
     </div>
   );
 };
