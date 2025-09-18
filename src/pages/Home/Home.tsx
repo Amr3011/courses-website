3be9754fbd335ea204e152fa29b902566d815b94
Mohamed Seed <mosaeed7888@gmail.com>
2025-09-18 06:46:56 +0300

    feat: create LearningExperience component with structured content and card hover features

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,11 @@
+import LearningExperience from "./Learning Experience/LearningExperience";
 
 const Home = () => {
   return (
-    <div className="text-primary-1">Home</div>
-  )
-}
+    <div className=" text-primary-1 my-48">
+      <LearningExperience />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
